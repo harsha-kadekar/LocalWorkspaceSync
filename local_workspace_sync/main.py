19842070da3a6b84b17d083ce64f9e915bbca2d5
Harsha Kadekar <harsha.kadekar@gmail.com>
2024-07-31 23:29:39 -0700

    add: add linters and formaters to the package

--- local_workspace_sync/main.py
+++ local_workspace_sync/main.py
@@ -1,3 +1,2 @@
-
 def add_numbers(var1, var2):
     return var1 + var2
--- tests/test_local_workspace_sync_executor.py
+++ tests/test_local_workspace_sync_executor.py
@@ -2,7 +2,6 @@ from local_workspace_sync import main
 
 
 class TestLocalWorkspaceSyncExecutor:
-
     def test_add_numbers(self):
         value = main.add_numbers(10, 20)
         assert value == 30
