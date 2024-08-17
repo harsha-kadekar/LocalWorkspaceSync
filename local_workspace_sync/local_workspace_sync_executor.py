634be303390627b2958e239c6c17086ca2cc3f12
Harsha Kadekar <harsha.kadekar@gmail.com>
2024-08-17 15:52:27 -0700

    chore: add unit test for main entry function

--- local_workspace_sync/local_workspace_sync_executor.py
+++ local_workspace_sync/local_workspace_sync_executor.py
@@ -4,3 +4,4 @@ from local_workspace_sync import main
 def main_entry():
     print("Hello World!")
     print(main.add_numbers(10, 20))
+    return 0
--- tests/test_local_workspace_sync_executor.py
+++ tests/test_local_workspace_sync_executor.py
@@ -1,3 +1,4 @@
+import local_workspace_sync.local_workspace_sync_executor
 from local_workspace_sync import main
 
 
@@ -5,3 +6,7 @@ class TestLocalWorkspaceSyncExecutor:
     def test_add_numbers(self):
         value = main.add_numbers(10, 20)
         assert value == 30
+
+    def test_main_entry(self):
+        return_value = local_workspace_sync.local_workspace_sync_executor.main_entry()
+        assert return_value == 0
